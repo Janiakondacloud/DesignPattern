3e1f473a9c1fdff195bd7981107398de7d0f2802
Janiak <69315011+Janiakondacloud@users.noreply.github.com>
2022-11-07 17:43:12 +0800

    优化类名

--- code/src/singleton/SingletonDemo1.java
+++ code/src/singleton/SingletonDemo1.java
@@ -1,27 +1,23 @@
 package singleton;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 /**
  * @Author: Janiak
  * @Date: 2022/11/07/15:33
  * @Description:
  */
-public class Demo1 {
+public class SingletonDemo1 {
     private int code;
     private String message;
-    private static Demo1 uniqueInstance = null;
+    private static SingletonDemo1 uniqueInstance = null;
 
-    private Demo1() {
+    private SingletonDemo1() {
         this.message = "你好";
         this.code = 1;
     }
 
-    public static Demo1 getInstance() {
+    public static SingletonDemo1 getInstance() {
         if (uniqueInstance == null) {
-            uniqueInstance = new Demo1();
+            uniqueInstance = new SingletonDemo1();
         }
         return uniqueInstance;
     }
